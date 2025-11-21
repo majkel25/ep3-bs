e252e14223428e2170648b55878e06141be5a595
Michael <Michael@milu.london>
2025-11-21 01:09:48 +0000

    First Try, added everything

---
+++ module/Service/src/Factory/BookingInterestServiceFactory.php
@@ -0,0 +1,33 @@
+<?php
+namespace Service\Factory;
+
+use Service\Service\BookingInterestService;
+use Service\Service\WhatsAppService;
+use Zend\Db\Adapter\Adapter;
+use Zend\Mail\Transport\TransportInterface;
+use Zend\ServiceManager\FactoryInterface;
+use Zend\ServiceManager\ServiceLocatorInterface;
+
+class BookingInterestServiceFactory implements FactoryInterface
+{
+    /**
+     * @param ServiceLocatorInterface $sl
+     * @return BookingInterestService
+     */
+    public function createService(ServiceLocatorInterface $sl)
+    {
+        /** @var Adapter $db */
+        $db = $sl->get('Zend\Db\Adapter\Adapter');
+
+        /** @var TransportInterface $mail */
+        $mail = $sl->get('Zend\Mail\Transport\TransportInterface');
+
+        $config  = $sl->get('config');
+        $mailCfg = isset($config['mail']) ? $config['mail'] : array();
+
+        /** @var WhatsAppService $wa */
+        $wa = $sl->get(WhatsAppService::class);
+
+        return new BookingInterestService($db, $mail, $mailCfg, $wa);
+    }
+}
