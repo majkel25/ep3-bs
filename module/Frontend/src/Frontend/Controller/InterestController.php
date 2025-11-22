425de66ab1121b7d59e28287ffb52f8dd809f8a5
Michael <Michael@milu.london>
2025-11-22 02:11:38 +0000

    aNOTHER BVERSION

--- module/Frontend/src/Frontend/Controller/InterestController.php
+++ module/Frontend/src/Frontend/Controller/InterestController.php
@@ -10,75 +10,88 @@ class InterestController extends AbstractActionController
 {
     public function registerAction()
     {
-        try {
-            $request = $this->getRequest();
+        $request = $this->getRequest();
 
-            if (! $request->isPost()) {
-                return new JsonModel(array(
-                    'ok'    => false,
-                    'error' => 'METHOD_NOT_ALLOWED',
-                ));
-            }
-
-            // Do NOT start a new session with default name; assume ep3-bs already did it.
-            // But if for some reason it's not started yet, start it without changing the name.
-            if (session_status() === PHP_SESSION_NONE) {
-                session_start();
-            }
-
-
-            $serviceManager     = $this->getServiceLocator();
-            $userSessionManager = $serviceManager->get('User\Manager\UserSessionManager');
-            $user               = $userSessionManager->getSessionUser();
-
-            if (! $user) {
-                // DEBUG: show what PHP thinks the session & cookies are
-                return new JsonModel(array(
-                    'ok'        => false,
-                    'error'     => 'AUTH_REQUIRED',
-                    'session'   => $_SESSION,
-                    'cookies'   => $_COOKIE,
-                ));
-            }
-
-            $dateStr = $this->params()->fromPost('date');
-
-            if (! $dateStr || ! preg_match('/^\d{4}-\d{2}-\d{2}$/', $dateStr)) {
-                return new JsonModel(array(
-                    'ok'    => false,
-                    'error' => 'INVALID_DATE',
-                ));
-            }
-
-            try {
-                $date = new \DateTime($dateStr);
-            } catch (\Exception $e) {
-                return new JsonModel(array(
-                    'ok'    => false,
-                    'error' => 'INVALID_DATE',
-                ));
-            }
-
-            // ep3-bs user entities normally store primary key as "uid"
-            $userId = (int) $user->need('uid');
-
-            /** @var BookingInterestService $svc */
-            $svc = $serviceManager->get(BookingInterestService::class);
-
-            $svc->registerInterest($userId, $date);
-
+        if (! $request->isPost()) {
             return new JsonModel(array(
-                'ok' => true,
-            ));
-        } catch (\Throwable $e) {
-            // TEMPORARY: expose the exception so we can see what's going on
-            return new JsonModel(array(
-                'ok'      => false,
-                'error'   => 'EXCEPTION',
-                'message' => $e->getMessage(),
-                'type'    => get_class($e),
+                'ok'    => false,
+                'error' => 'METHOD_NOT_ALLOWED',
             ));
         }
+
+        // --- make sure we are attached to the SAME session as ep3-bs uses ---
+        $serviceManager = $this->getServiceLocator();
+        $config         = $serviceManager->get('Config');
+
+        $sessionName = null;
+        if (isset($config['session']['config']['name'])
+            && is_string($config['session']['config']['name'])
+            && $config['session']['config']['name'] !== ''
+        ) {
+            $sessionName = $config['session']['config']['name'];
+        }
+
+        if ($sessionName !== null) {
+            // If PHP is using a different session name, switch to the ep3 one
+            if (session_name() !== $sessionName) {
+                if (session_status() === PHP_SESSION_ACTIVE) {
+                    session_write_close();
+                }
+                session_name($sessionName);
+            }
+        }
+
+        if (session_status() !== PHP_SESSION_ACTIVE) {
+            @session_start();
+        }
+        // --- end session attach ---
+
+        // Get logged-in user via UserSessionManager (standard ep3-bs way)
+        $userSessionManager = $serviceManager->get('User\Manager\UserSessionManager');
+        $user               = $userSessionManager->getSessionUser();
+
+        if (! $user) {
+            return new JsonModel(array(
+                'ok'    => false,
+                'error' => 'AUTH_REQUIRED',
+            ));
+        }
+
+        $dateStr = $this->params()->fromPost('date');
+
+        if (! $dateStr || ! preg_match('/^\d{4}-\d{2}-\d{2}$/', $dateStr)) {
+            return new JsonModel(array(
+                'ok'    => false,
+                'error' => 'INVALID_DATE',
+            ));
+        }
+
+        try {
+            $date = new \DateTime($dateStr);
+        } catch (\Exception $e) {
+            return new JsonModel(array(
+                'ok'    => false,
+                'error' => 'INVALID_DATE',
+            ));
+        }
+
+        // ep3-bs user entities normally store primary key as "uid"
+        $userId = (int) $user->need('uid');
+
+        /** @var BookingInterestService $svc */
+        $svc = $serviceManager->get(BookingInterestService::class);
+
+        try {
+            $svc->registerInterest($userId, $date);
+        } catch (\Exception $e) {
+            return new JsonModel(array(
+                'ok'    => false,
+                'error' => 'SERVER_ERROR',
+            ));
+        }
+
+        return new JsonModel(array(
+            'ok' => true,
+        ));
     }
 }
-// testing 2
