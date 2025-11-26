66dbb20aaffa2d1264354c1232b8036135ee2da5
Michael <Michael@milu.london>
2025-11-26 18:22:17 +0000

    Update ep3-bs application code

--- module/Booking/src/Booking/Service/BookingServiceFactory.php
+++ module/Booking/src/Booking/Service/BookingServiceFactory.php
@@ -4,14 +4,17 @@ namespace Booking\Service;
 
 use Zend\ServiceManager\FactoryInterface;
 use Zend\ServiceManager\ServiceLocatorInterface;
+use Zend\EventManager\EventManagerInterface;
+use Throwable;
 
 class BookingServiceFactory implements FactoryInterface
 {
-
+    /**
+     * {@inheritdoc}
+     */
     public function createService(ServiceLocatorInterface $sm)
     {
-        $notificationListener = $sm->get('Booking\Service\Listener\NotificationListener');
-
+        // Create the core BookingService with all required dependencies
         $bookingService = new BookingService(
             $sm->get('Base\Manager\OptionManager'),
             $sm->get('Booking\Manager\BookingManager'),
@@ -19,12 +22,24 @@ class BookingServiceFactory implements FactoryInterface
             $sm->get('Booking\Manager\ReservationManager'),
             $sm->get('Square\Manager\SquarePricingManager'),
             $sm->get('ViewHelperManager'),
-            $sm->get('Zend\Db\Adapter\Adapter')->getDriver()->getConnection());
+            $sm->get('Zend\Db\Adapter\Adapter')->getDriver()->getConnection()
+        );
 
-        $eventManager = $bookingService->getEventManager();
-        $eventManager->attach($notificationListener);
+        // Try to attach the notification listener, but NEVER let this
+        // block creation of the BookingService itself.
+        try {
+            /** @var EventManagerInterface $eventManager */
+            $eventManager = $bookingService->getEventManager();
+
+            $notificationListener = $sm->get('Booking\Service\Listener\NotificationListener');
+
+            // Attach listener to the booking events
+            $eventManager->attach($notificationListener);
+        } catch (Throwable $e) {
+            // If anything goes wrong with the listener, we simply skip it.
+            // You can later add logging here if you want to inspect $e.
+        }
 
         return $bookingService;
     }
-
 }
